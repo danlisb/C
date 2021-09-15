ab5e639b4104f1b089dccbf8c4c1c21c9c34ed7c
danlisb <86170130+danlisb@users.noreply.github.com>
2021-09-15 17:11:51 -0300

    Add files via upload

--- Ex5.c
+++ Ex5.c
@@ -3,27 +3,31 @@
 #include <math.h>
 #include <string.h>
 #include <locale.h>
-/*Faça um algoritmo que leia três notas de um aluno, calcule e escreva a média final
-deste aluno. Considerar que a média é ponderada e que o peso das notas é 2, 3 e 5.*/
 
 int main()
-    {setlocale(LC_ALL, "Portuguese_Brazil");
+{
+setlocale(LC_ALL, "Portuguese_Brazil");
 
-    float n1,n2,n3, media;
+int idade;
 
-    printf("Insira o valor da nota 1: ");
-    scanf("%f", &n1);
+printf("Digite a idade: ");
+scanf("%i",&idade);
 
-    printf("Insira o valor da nota 2: ");
-    scanf("%f", &n2);
-
-    printf("Insira o valor da nota 3: ");
-    scanf("%f", &n3);
-
-    media = (n1*2+n2*3+n3*5)/10;
-
-    printf("O valor da média é: %.1f", media);
-
+if (idade >= 5 && idade <= 7){
+    printf("Classificação: Infantil A\n");
+}
+if (idade >= 8 && idade <= 10){
+    printf("Classificação: Infantil B\n");
+}
+if (idade >= 11 && idade <= 13){
+    printf("Classificação: Juvenil	A\n");
+}
+if (idade >= 14 && idade <= 17){
+    printf("Classificação: Juvenil	B\n");
+}
+if (idade >= 18){
+    printf("Classificação: Adulto\n");
+}
 
 return 0;
 }

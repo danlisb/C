24eff3f83ecba702606f56f03edf2ec38517a3b4
danlisb <86170130+danlisb@users.noreply.github.com>
2021-09-15 17:08:42 -0300

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <locale.h>
+/*Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular
+e escrever a área do retângulo*/
+
+int main()
+    {setlocale(LC_ALL, "Portuguese_Brazil");
+
+    int b, h, area;
+
+    printf("Digite o valor da base: ");
+    scanf("%d", &b);
+
+    printf("Digite o valor da altura: ");
+    scanf("%d", &h);
+
+    area = b*h;
+
+    printf("O valor da área é: %d", area);
+
+return 0;
+}
